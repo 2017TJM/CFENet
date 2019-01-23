8a7c597177fc6a4491ba70587dfbf99ec17df5cd
Yiming992 <yimingliuno1@gmail.com>
2019-01-23 18:29:13 +0800

    create project

---
+++ base_model.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np 
+import torch.nn as nn 
+import os
+from torchvision import models 
+
+class Vgg16(nn.Module):
+
+    def __init__(self):
+        super(Vgg16,self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class ResNet(nn.Module):
+
+    def __init__(self):
+        super(ResNet,self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+
+
+
+
+
+
